0e9142e2c1b0bdc1880633eeadee9b49ca979c4c
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 01:21:44 +0800

    ✨ feat(app): integrate new modules into app module
    - import and register price, location and service modules
    - organize module imports for better readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
-import { SubCategoryModule } from './sub-category/sub-category.module';
 import { CategoryModule } from './category/category.module';
+import { SubCategoryModule } from './sub-category/sub-category.module';
 import { PriceModule } from './price/price.module';
 import { LocationModule } from './location/location.module';
 import { ServiceModule } from './service/service.module';
 
 @Module({
-  imports: [SubCategoryModule, CategoryModule],
+  imports: [
+    CategoryModule,
+    SubCategoryModule,
+    PriceModule,
+    LocationModule,
+    ServiceModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
 })
 export class AppModule {}
+
