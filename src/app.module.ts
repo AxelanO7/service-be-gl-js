3b11f632109fbed8998f3d6ca10b24394ef2f36a
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 01:26:56 +0800

    ♻️ refactor(app): remove unused modules
    - remove PriceModule and LocationModule
    - these modules are no longer needed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,12 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { CategoryModule } from './category/category.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
-import { PriceModule } from './price/price.module';
-import { LocationModule } from './location/location.module';
 import { ServiceModule } from './service/service.module';
 
 @Module({
-  imports: [
-    CategoryModule,
-    SubCategoryModule,
-    PriceModule,
-    LocationModule,
-    ServiceModule,
-  ],
+  imports: [CategoryModule, SubCategoryModule, ServiceModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
 })
 export class AppModule {}
-
