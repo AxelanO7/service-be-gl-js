cf7f8ffe2dd6b88b0749ab9db2e8014dd67701c7
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 01:11:42 +0800

    ✨ feat(app): integrate subcategory module
    - import and register subcategory module in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { LocationModule } from './location/location.module';
 import { PriceModule } from './price/price.module';
 
 @Module({
-  imports: [CategoryModule],
+  imports: [CategoryModule, SubCategoryModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
