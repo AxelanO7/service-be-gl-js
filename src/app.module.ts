c97c6b66eb39cbb13f6b99310472bfdf667ef2e7
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-29 23:50:49 +0800

    ✨ feat(category): implement category module
    - add category module with CRUD operations
    - integrate prisma for database interaction

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { ServiceModule } from './service/service.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [ServiceModule],
+  imports: [ServiceModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
---
+++ src/category/category.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma.service';
+import { Prisma } from 'src/prisma';
+
+@Injectable()
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return await this.prisma.category.create({
+      data: createCategoryDto as Prisma.CategoryCreateInput,
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.category.findMany();
+  }
+
+  async findOne(id: string) {
+    return await this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return await this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto as Prisma.CategoryUpdateInput,
+    });
+  }
+
+  async remove(id: string) {
+    return await this.prisma.category.delete({
+      where: { id },
+    });
+  }
+}
