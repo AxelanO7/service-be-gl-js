46f39c9b59fd0073b0743a92a95bad236de35f98
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 22:47:34 +0800

    ✨ feat(category): implement category management endpoints - enable create, read, update, and delete operations for categories - add controller methods and service functions to handle category requests - integrate with prisma to manage category data in the database
    🐛 fix(prisma): update prisma client import
    
    - fix import path for PrismaClient to resolve module not found error

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
   constructor(private readonly prisma: PrismaService) {}
-  // async create(createCategoryDto: CreateCategoryDto) {
-  //   return await this.prisma.category.create({
-  //     data: createCategoryDto as Prisma.CategoryCreateInput,
-  //   });
-  // }
-  // async findAll() {
-  //   return await this.prisma.category.findMany();
-  // }
-  // async findOne(id: string) {
-  //   return await this.prisma.category.findUnique({
-  //     where: { id },
-  //   });
-  // }
-  // async update(id: string, updateCategoryDto: UpdateCategoryDto) {
-  //   return await this.prisma.category.update({
-  //     where: { id },
-  //     data: updateCategoryDto as Prisma.CategoryUpdateInput,
-  //   });
-  // }
-  // async remove(id: string) {
-  //   return await this.prisma.category.delete({
-  //     where: { id },
-  //   });
-  // }
+  async create(createCategoryDto: CreateCategoryDto) {
+    return await this.prisma.category.create({
+      data: createCategoryDto as Prisma.CategoryCreateInput,
+    });
+  }
+  async findAll() {
+    return await this.prisma.category.findMany();
+  }
+  async findOne(id: string) {
+    return await this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return await this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto as Prisma.CategoryUpdateInput,
+    });
+  }
+  async remove(id: string) {
+    return await this.prisma.category.delete({
+      where: { id },
+    });
+  }
 }
