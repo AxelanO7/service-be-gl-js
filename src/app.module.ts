acee88f96c1b87a6fdddaad663c2067e21b8bdd6
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 16:28:29 +0800

    🔥 feat(app): streamline app module imports
    - remove unused location module import to improve code clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { CategoryModule } from './category/category.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
-import { LocationModule } from './location/location.module';
 
 @Module({
   imports: [CategoryModule, SubCategoryModule],
