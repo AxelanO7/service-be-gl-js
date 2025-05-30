d6a1cda268e757687590712d093e2fef6b92476f
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 22:20:18 +0800

    ♻️ refactor(module): remove PrismaService from module providers
    - remove PrismaService from category, location, service and sub-category modules
    - the PrismaService is already provided globally

--- src/service/service.module.ts
+++ src/service/service.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ServiceService } from './service.service';
 import { ServiceController } from './service.controller';
-import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [ServiceController],
-  providers: [ServiceService, PrismaService],
+  providers: [ServiceService],
 })
 export class ServiceModule {}
--- src/sub-category/sub-category.module.ts
+++ src/sub-category/sub-category.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SubCategoryService } from './sub-category.service';
 import { SubCategoryController } from './sub-category.controller';
-import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [SubCategoryController],
-  providers: [SubCategoryService, PrismaService],
+  providers: [SubCategoryService],
 })
 export class SubCategoryModule {}
