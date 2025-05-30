0d0b96b9b3e03a350d7d47e8d991ea681fb6aaa4
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 23:24:27 +0800

    ✨ feat(app): integrate location and price modules
    - import and integrate the LocationModule
    - import and integrate the PriceModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SubCategory } from './sub-category/entities/sub-category.entity';
+import { PriceModule } from './price/price.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
-  imports: [CategoryModule, SubCategory, PrismaModule],
+  imports: [
+    CategoryModule,
+    SubCategory,
+    LocationModule,
+    PriceModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [PrismaModule],
