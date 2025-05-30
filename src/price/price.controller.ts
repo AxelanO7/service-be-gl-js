3368e1b778884b8034c7998c1fbc729963aa8b53
AxelanO7 <jeremia123.jm@gmail.com>
2025-05-30 17:34:37 +0800

    ✨ feat(price): implement price management feature
    - enable create, read, update, and delete operations for prices
    - integrate with prisma for database interactions

--- src/price/price.controller.ts
+++ src/price/price.controller.ts
@@ -1,26 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreatePriceDto } from './dto/create-price.dto';
+import { UpdatePriceDto } from './dto/update-price.dto';
+import { PriceService } from './price.service';
 
 @Controller('price')
 export class PriceController {
-  // constructor(private readonly priceService: PriceService) {}
-  // @Post()
-  // create(@Body() createPriceDto: CreatePriceDto) {
-  //   return this.priceService.create(createPriceDto);
-  // }
-  // @Get()
-  // findAll() {
-  //   return this.priceService.findAll();
-  // }
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.priceService.findOne(id);
-  // }
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updatePriceDto: UpdatePriceDto) {
-  //   return this.priceService.update(id, updatePriceDto);
-  // }
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.priceService.remove(id);
-  // }
+  constructor(private readonly priceService: PriceService) {}
+  @Post()
+  create(@Body() createPriceDto: CreatePriceDto) {
+    return this.priceService.create(createPriceDto);
+  }
+  @Get()
+  findAll() {
+    return this.priceService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.priceService.findOne(id);
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePriceDto: UpdatePriceDto) {
+    return this.priceService.update(id, updatePriceDto);
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.priceService.remove(id);
+  }
 }
--- src/price/price.service.ts
+++ src/price/price.service.ts
@@ -1,30 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { CreatePriceDto } from './dto/create-price.dto';
+import { UpdatePriceDto } from './dto/update-price.dto';
 
 @Injectable()
 export class PriceService {
-  // constructor(private readonly prisma: PrismaService) {}
-  // async create(createPriceDto: CreatePriceDto) {
-  //   return await this.prisma.price.create({
-  //     data: createPriceDto as Prisma.PriceCreateInput,
-  //   });
-  // }
-  // async findAll() {
-  //   return await this.prisma.price.findMany();
-  // }
-  // async findOne(id: string) {
-  //   return await this.prisma.price.findUnique({
-  //     where: { id },
-  //   });
-  // }
-  // async update(id: string, updatePriceDto: UpdatePriceDto) {
-  //   return await this.prisma.price.update({
-  //     where: { id },
-  //     data: updatePriceDto as Prisma.PriceUpdateInput,
-  //   });
-  // }
-  // async remove(id: string) {
-  //   return await this.prisma.price.delete({
-  //     where: { id },
-  //   });
-  // }
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createPriceDto: CreatePriceDto) {
+    return await this.prisma.price.create({
+      data: createPriceDto as Prisma.PriceCreateInput,
+    });
+  }
+  async findAll() {
+    return await this.prisma.price.findMany();
+  }
+  async findOne(id: string) {
+    return await this.prisma.price.findUnique({
+      where: { id },
+    });
+  }
+  async update(id: string, updatePriceDto: UpdatePriceDto) {
+    return await this.prisma.price.update({
+      where: { id },
+      data: updatePriceDto as Prisma.PriceUpdateInput,
+    });
+  }
+  async remove(id: string) {
+    return await this.prisma.price.delete({
+      where: { id },
+    });
+  }
 }
