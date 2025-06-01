543160ee20880a70ce463795ec4e17856e7881da
AxelanO7 <jeremia123.jm@gmail.com>
2025-06-02 02:37:19 +0800

    ♻️ refactor(app): refactor subcategory module import
    - import the SubCategoryModule instead of the SubCategory entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { SubCategory } from './sub-category/entities/sub-category.entity';
 import { PriceModule } from './price/price.module';
 import { LocationModule } from './location/location.module';
+import { SubCategoryModule } from './sub-category/sub-category.module';
 
 @Module({
   imports: [
     CategoryModule,
-    SubCategory,
+    SubCategoryModule,
     LocationModule,
     PriceModule,
     PrismaModule,
